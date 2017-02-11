e7535b6a0abdf63234ea11d5cd570a494a7e9381
Armando Cerrillo <armando@cerrillomedia.org>
2017-02-11 17:12:12 +0000

    modify main_menu with search option

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -8,36 +8,40 @@ class MenuController
     end
     
     def main_menu
-        puts "Main Menu: [ #{address_book.entries.count} ] entries"
+        puts "Main Menu: [ #{@address_book.entries.count} ] entries"
         puts "[1] View all entries"
         puts "[2] Create an entry"
         puts "[3] Search for an entry"
         puts "[4] Import entries from CSV"
-        puts "[5] Exit"
+        puts "[5] retrieve entry by number"
+        puts "[6] Exit"
         print "Enter your selection: "
         
-        selection = gets.to_i
+        selection = gets.chomp
         
         case selection
-            when 1
+            when "1"
                 system "clear"
                 view_all_entries
                 main_menu
-            when 2
+            when "2"
                 system "clear"
                 create_entry
                 main_menu
-            when 3
+            when "3"
                 system "clear"
                 search_entries
                 main_menu
-            when 4
+            when "4"
                 system "clear"
                 read_csv
                 main_menu
-            when 5
+            when "5"
+                system "clear"
+                view_entry_by_num
+                main_menu
+            when "6"
                 puts "Good-bye!"
-                
                 exit(0)
             else
                 system "clear"
@@ -49,7 +53,7 @@ class MenuController
     def view_all_entries
         system "clear"
         
-        address_book.entries.each do |entry|
+        @address_book.entries.each do |entry|
            system "clear"
            puts entry.to_s
            
@@ -60,7 +64,7 @@ class MenuController
     
     def create_entry
         system "clear"
-        "New AddressBloc Entry"
+        puts "Enter data for entry ##{@address_book.entries.length + 1}"
         print "Name: "
         name = gets.chomp
         print "Phone Number: "
@@ -68,10 +72,10 @@ class MenuController
         print "Email: "
         email = gets.chomp
         
-        address_book.add_entry(name, phone, email)
+        @address_book.add_entry(name, phone, email)
     
         system "clear"
-        puts "New entry created"
+        puts "entry #{@address_book.entries.length} created"
     end
     
     def search_entries
@@ -107,5 +111,28 @@ class MenuController
         end
                 
     end
+    
+    def view_entry_by_num
+        # allow for access to array greater than 0
+        # length = address_book.entries.length
+        print "Choose an entry: "
+        entry = gets.chomp.to_i
         
+        # if no entries
+        if address_book.entries.length == 0
+            puts "address book empty. Press 'enter' to return to main menu"
+            gets.chomp
+            system "clear"
+            main_menu
+        elsif address_book.entries[entry-1].nil? || entry < 0
+            puts "#{entry} is not a valid entry.\nPress 'enter' to search another entry"
+            gets.chomp
+            system "clear"
+            view_entry_by_num
+        end
+        
+        puts address_book.entries[entry-1].to_s
+        entry_submenu(entry-1)
+    end
+    
 end
